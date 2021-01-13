966763e7bcdae8af76f0158e1bc933d8703be03f
lodis <lodisperkins44@gmail.com>
2021-01-13 13:51:09 -0600

    Added bullet script and prefab

--- Assets/Scripts/Gameplay/BulletBehavior.cs
+++ Assets/Scripts/Gameplay/BulletBehavior.cs
@@ -9,32 +9,44 @@ public class BulletBehavior : MonoBehaviour
     [SerializeField]
     private float _damage;
 
-    public Vector3 velocity;
+    private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Grabs the rigidbody component attached to the bullet.
+        _rigidbody = GetComponent<Rigidbody>();
+
+        //Deletes the bullet after the given amount of time has passed.
+        Destroy(gameObject, _despawnTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Tries to find the health script on ther game object it collided with.
         HealthBehavior otherHealth = other.GetComponent<HealthBehavior>();
 
-        if (otherHealth == null)
+        /// If the GetComponent function cannot find a health script on the object,
+        /// it will set the variable we created to be null. If we check if the variable is not null,
+        /// we'll be sure that our variable actually has a value in it before using it.
+        if (otherHealth != null)
         {
-            Destroy(gameObject);
-        }
-
-        else
-        {
+            ///Calls the TakeDamage function for the object the bullet collided with. 
+            ///Putting the bullets damage value in parenthesis tells the object to use
+            ///the bullets damage amount to decrease the health.
             otherHealth.TakeDamage(_damage);
         }
+
+        //Deletes the bullet once it has collided with an object.
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Called when ever a bullet is going to be fired. Applies a force to the 
+    /// gameObject using the velocity given.
+    /// </summary>
+    public void AddForce(Vector3 velocity)
     {
-        
+        _rigidbody.AddForce(velocity, ForceMode.Impulse);
     }
 }
